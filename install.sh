f6e7e919c64c3c351f53b1020f74b9f95bb50585
tim.tang <tang.jilong@139.com>
2013-11-28 20:38:13 +0800

    Update install shell script.

--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
 #Authore: tim.tang
 
-i##TODO:
+## - Install preparation.
+
+#  - Upgrade system
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# - Generate ssh-key
+echo -e "\n"|ssh-keygen -t rsa -C "tang.jilong@139.com"
+echo "Press Enter Key to Continue..." && read
+
+# - Authenticate ssh-key with GitHub
+echo "Authenticate With GitHub..." && yes|ssh -T git@github.com
+
+## TODO: checking continue or abort installation. 
+
+## - Install Git 
+
+## - Install Oh-My-ZSH
+
+## - Install CoreUtils
+
+## - Install MIsc
+
+## - Install MongoDB
+
+## - Install NodeJS
+
+## - Housekeeping
+cp gnzh.zsh-theme ~/.oh-my-zsh/themes/
+source ~/.zshrc
+sudo shutdown 0  -r
