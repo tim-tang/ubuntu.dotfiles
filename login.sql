bb6adda95918e8fe0bcd8149433943709802ad70
tim.tang <tang.jilong@139.com>
2014-01-06 08:26:58 +0800

    Update login.sql.

--- login.sql
+++ login.sql
@@ -1,12 +1,17 @@
 set sqlprompt "_USER'@'_CONNECT_IDENTIFIER'➠ '";
 set termout off
-set serveroutput on size unlimited
-set pagesize 50000
-set linesize 135
-set long 50000
+set serveroutput on size 600
+set pagesize 200
+set linesize 388
+set long 5000
 set trimspool on
 set tab off
 set timing on
 set trimout on
+set space 1
+SET CHARWIDTH 20
+SET COLSEP "|"
+SET RECSEP WRAPPED
+SET RECSEPCHAR "-"
 def _editor = vim
 ALTER SESSION SET nls_date_format = "mm/dd/yyyy hh24:mi:ss";
